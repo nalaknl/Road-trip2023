6875b079abae3f6eecb858b4636e8dc52d08e5cf
nalaknl <sebastien.leriche@hotmail.fr>
2023-01-15 01:38:08 +0100

    premier depot

---
+++ index.js
@@ -0,0 +1,8 @@
+// const carouselImages = document.querySelectorAll('.carousel-image');
+// carouselImages.forEach(img => img.style.display = 'none');
+
+// setInterval(function() {
+//   let randomIndex = Math.floor(Math.random() * carouselImages.length);
+//   carouselImages.forEach(img => img.style.display = 'none');
+//   carouselImages[randomIndex].style.display = 'block';
+// },3000);
